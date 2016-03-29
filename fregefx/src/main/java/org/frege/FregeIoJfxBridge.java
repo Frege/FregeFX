5a9ac2ee8f400c7a49af3f0d724c9383e2114e01
Dierk Koenig <dierk.koenig@canoo.com>
2016-03-29 13:36:49 +0200

    make bridgeAction use async for io

--- fregefx/src/main/java/org/frege/FregeIoJfxBridge.java
+++ fregefx/src/main/java/org/frege/FregeIoJfxBridge.java
@@ -3,12 +3,13 @@ package org.frege;
 import frege.runtime.Applicable;
 import frege.runtime.Delayed;
 import frege.runtime.Lambda;
-import javafx.event.Event;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+
+import java.util.concurrent.*;
 
 public class FregeIoJfxBridge {
-    protected Lambda ioLambda;
-    protected Lambda jfxLambda;
+    protected Lambda ioLambda;  // to be executed async
+    protected Lambda jfxLambda; // to be executed in JavaFx UI Application thread
 
     public FregeIoJfxBridge(Lambda ioLambda, Lambda jfxLambda) {
         this.ioLambda = ioLambda;
@@ -17,10 +18,27 @@ public class FregeIoJfxBridge {
 
     public void bridge() {
         try {
-            Applicable inter = ioLambda.apply(null); // no first argument
-            final Object ioResult = Delayed.forced(inter.apply(null).result().forced());// the second argument is the IO context
-            Applicable inter2 = jfxLambda.apply(ioResult); // no first argument
-            Delayed.forced(inter2.apply(null).result().forced());// the second argument is the JFX context
+
+            final FutureTask<Object> ioFuture =
+                new FutureTask<Object>(new Callable<Object>() {
+                    public Object call() {
+                        Applicable inter = ioLambda.apply(null); // no first argument
+                        final Object ioResult = Delayed.forced(inter.apply(null).result().forced());// the second argument is the IO context
+
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                Applicable inter2 = jfxLambda.apply(ioResult);
+                                Delayed.forced(inter2.apply(null).result().forced());// the second argument is the JFX context
+                            }
+                        });
+                        return null;
+                    }
+                });
+
+            ForkJoinPool.commonPool().execute(ioFuture);
+
+
         } catch(RuntimeException re) {
             re.printStackTrace();
             throw re;
